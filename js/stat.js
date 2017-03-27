bff2a021b863f97c2b274787ed4591188f56cbeb
LarisaK <lloraklets@gmail.com>
2017-03-27 20:33:02 +0900

    отрисовка прямоугольника оформлена функцией

--- js/stat.js
+++ js/stat.js
@@ -1,16 +1,21 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(20, 30, 420, 270);
+  var drawRect = function (colorRect, x, y, width, height) {
+    ctx.fillStyle = colorRect;
+    ctx.fillRect(x, y, width, height);
+  };
+  drawRect('rgba(0, 0, 0, 0.7)', 20, 30, 420, 270);
+  
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-  ctx.strokeRect(10, 10, 420, 270);
   ctx.fillRect(10, 10, 420, 270);
+  ctx.strokeRect(10, 10, 420, 270);
+  
   ctx.fillStyle = '#000';
   ctx.font = '14px PT Mono';
-
   ctx.fillText('Ура, вы победили!', 25, 35);
   ctx.fillText('Список результатов:', 25, 56);
+  
   var max = -1;
 
   for (var i = 0; i < times.length; i++) {
@@ -20,24 +25,23 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 
+  var histoWidth = 40;            //ширина столбика
   var histoHeight = 160;          // максим. столбик
   var histoX = 40;                // начальная координата x
   var step = histoHeight / max;   // коэф. для вычисл. высоты столбика
   var columnIndent = 90;          // ширина  столбика + отступ
   var bottomY = 240;              // низ гистограммы
-
+ 
   for (i = 0; i < times.length; i++) {
     var name = names[i];
-    time = times[i];
-    var height = step * time;
+    var height = step * times[i];
     if (name === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      drawRect('rgba(255, 0, 0, 1)', histoX + columnIndent * i, bottomY - height, histoWidth, height);
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toFixed(1) + ')';
+        drawRect('rgba(0, 0, 255, ' + Math.random().toFixed(1) + ')', histoX + columnIndent * i, bottomY - height, histoWidth, height);
     }
-    ctx.fillRect(histoX + columnIndent * i, bottomY - height, 40, height);
     ctx.fillStyle = '#000';
-    ctx.fillText(time.toFixed(0), histoX + columnIndent * i + 8, bottomY - height - 6);
+    ctx.fillText(times[i].toFixed(0), histoX + columnIndent * i + 8, bottomY - height - 6);
     // попробуем написать имя по центру столбика, считая, что по ширине столбика помещаются 6 букв
     var lengthOfName = name.length;
     if (lengthOfName < 6) {
@@ -48,5 +52,3 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillText(name, histoX + columnIndent * i, bottomY + 18);
   }
 };
-var canvas = document.querySelector('canvas');  // сделаем вызов функции, посмотреть
-renderStatistics(canvas.getContext('2d'), ['Иван', 'Игнат', 'Вы', 'Максимилиан'], [200.15, 418.32, 120.90, 316.1]);
